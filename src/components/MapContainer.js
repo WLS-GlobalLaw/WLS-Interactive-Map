3cb06f49d469b0faeb5d1a942f5e02e5e31f035f
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-05-09 21:10:13 -0400

    added new map tile for testing purposes

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,6 +3,8 @@ import { Map, TileLayer, GeoJSON } from "react-leaflet";
 
 import "../styles/MapContainer.scss";
 
+const URL = `https://api.mapbox.com/styles/v1/chrisstanarsenault/cka0aphis04f51insdpc0zjfn.html/tiles/1/1/0?fresh=true&title=copy&access_token=pk.eyJ1IjoiY2hyaXNzdGFuYXJzZW5hdWx0IiwiYSI6ImNrOWY1dDV4NjA5bnczZW9iNXB4dmkzcHIifQ.03UOcpgGEfNQbYGxuVBkBQ`;
+
 export default class MapContainer extends Component {
   constructor(props) {
     super(props);
@@ -13,15 +15,17 @@ export default class MapContainer extends Component {
   render() {
     return (
       <Map center={this.state.position} zoom={1} id="map" className="map">
-        {console.log(process.env.REACT_APP_ACCESS_TOKEN)}
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`}
+          // url={URL}
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
           minZoom={3}
           maxZoom={10}
           zoomDelta={0.1}
           zoomSnap={0}
-          id="mapbox/streets-v11"
+          //id="mapbox/streets-v11"
+          //id="chrisstanarsenault/cka0bh3lk13qg1it67mcpj4gg"
+          id="chrisstanarsenault/cka0cv5op0e971ipj4a8qqnax"
           tileSize={512}
           zoomOffset={-1}
           accessToken={process.env.REACT_APP_ACCESS_TOKEN}
