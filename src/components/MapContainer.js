dc0e26c25f8a484f283ac173175d3f5708eb0de4
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-05-09 20:00:50 -0400

    added .env file and added public access token to it

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -13,8 +13,9 @@ export default class MapContainer extends Component {
   render() {
     return (
       <Map center={this.state.position} zoom={1} id="map" className="map">
+        {console.log(process.env.REACT_APP_ACCESS_TOKEN)}
         <TileLayer
-          url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hyaXNzdGFuYXJzZW5hdWx0IiwiYSI6ImNrOWY1dDV4NjA5bnczZW9iNXB4dmkzcHIifQ.03UOcpgGEfNQbYGxuVBkBQ`}
+          url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`}
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
           minZoom={3}
           maxZoom={10}
@@ -23,7 +24,7 @@ export default class MapContainer extends Component {
           id="mapbox/streets-v11"
           tileSize={512}
           zoomOffset={-1}
-          accessToken="pk.eyJ1IjoiY2hyaXNzdGFuYXJzZW5hdWx0IiwiYSI6ImNrOWY1dDV4NjA5bnczZW9iNXB4dmkzcHIifQ.03UOcpgGEfNQbYGxuVBkBQ"
+          accessToken={process.env.REACT_APP_ACCESS_TOKEN}
         />
       </Map>
     );
