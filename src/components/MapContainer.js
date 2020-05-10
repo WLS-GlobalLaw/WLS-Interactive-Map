8c6dfa46495adf7821abd4c6302033b20b2ed320
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-05-09 21:15:39 -0400

    removed process.env for access token

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,7 +3,7 @@ import { Map, TileLayer, GeoJSON } from "react-leaflet";
 
 import "../styles/MapContainer.scss";
 
-const URL = `https://api.mapbox.com/styles/v1/chrisstanarsenault/cka0aphis04f51insdpc0zjfn.html/tiles/1/1/0?fresh=true&title=copy&access_token=pk.eyJ1IjoiY2hyaXNzdGFuYXJzZW5hdWx0IiwiYSI6ImNrOWY1dDV4NjA5bnczZW9iNXB4dmkzcHIifQ.03UOcpgGEfNQbYGxuVBkBQ`;
+const pub_access_token = `pk.eyJ1IjoiY2hyaXNzdGFuYXJzZW5hdWx0IiwiYSI6ImNrOWY1dDV4NjA5bnczZW9iNXB4dmkzcHIifQ.03UOcpgGEfNQbYGxuVBkBQ`;
 
 export default class MapContainer extends Component {
   constructor(props) {
@@ -16,8 +16,7 @@ export default class MapContainer extends Component {
     return (
       <Map center={this.state.position} zoom={1} id="map" className="map">
         <TileLayer
-          url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`}
-          // url={URL}
+          url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${pub_access_token}`}
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
           minZoom={3}
           maxZoom={10}
