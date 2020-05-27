fcefd35738f0a5e5e084b03c4638ff83def6de4e
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-05-26 23:06:32 -0400

    added new endpoint country-data for collecting googlesheet information to use on frontend

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 const app = express();
 const router = express.Router();
@@ -11,6 +13,8 @@ const lawDgJson = require("./lawDgJson.json");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+console.log(process.env.REACT_APP_GOOGLE_PRIVATE_KEY);
 
 // API Calls
 router.get("/api/law-ds", (req, res) => {
@@ -28,6 +32,31 @@ router.get("/api/law-as", (req, res) => {
 router.get("/api/law-dg", (req, res) => {
   res.send({
     lawDgJson,
+  });
+});
+
+router.get("/api/country-data", async (req, res) => {
+  const doc = new GoogleSpreadsheet(
+    "1e-VbcEbz8-7RifDaD5B7rFE_k6N4o-eziUIZQyEur2Q"
+  );
+  // await doc.useServiceAccountAuth(creds);
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  });
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+  const info = rows.map((row) => ({
+    country: row.Country,
+    continent: row.Continent,
+    bodyText: row["Body Text"],
+    lawCategory: row["Law Category"],
+    description: row.Description,
+  }));
+
+  res.send({
+    countryInfo: info,
   });
 });
 
