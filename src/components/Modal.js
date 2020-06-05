68877534cf3fa0be8f43826320af4474599d3ad9
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-06-05 00:17:48 -0400

    changed the border colors of the modal to a dark grey as well as the close button to a dark blue.  Also put law data into a table

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -19,11 +19,22 @@ export default class Modal extends Component {
 
             <div>
               <h3>Existing Bills, Laws, and Standards</h3>
-              {this.props.googleSheetInfo.map((item, index) => (
-                <p key={index}>
-                  {item.billsAndLaws}: {item.description}
-                </p>
+              <table>
+                <thead>
+                  
+                  {this.props.googleSheetInfo.map((item, index) => (
+                // <p key={index}>
+                //   {item.billsAndLaws}: {item.description}
+                // </p>
+                <tr key={index} style={{border:"1px solid black"}}>
+                  <td border={1}>{item.billsAndLaws}</td>
+                  <td>{item.description}</td>
+                </tr>
               ))}
+                  
+                </thead>
+              </table>
+              
             </div>
           </div>
         </div>
