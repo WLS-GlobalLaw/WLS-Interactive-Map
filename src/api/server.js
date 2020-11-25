81bd0c9ff52209fecd070621be50ff66ed6f710c
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-11-25 03:33:06 -0500

    changed Country to Pillar in button container as per Aileen.  Fixed up row titles in server.js to reflex new titles for Type of Legislation and Title of Law

--- src/api/server.js
+++ src/api/server.js
@@ -1,45 +1,30 @@
-require("dotenv").config();
-const express = require("express");
-const serverless = require("serverless-http");
-const bodyParser = require("body-parser");
-const { GoogleSpreadsheet } = require("google-spreadsheet");
-const encoding = require("encoding"); // this is added (but not used) because of a netlify bug not including this file into the CLI.  They will add in a future patch.
+require('dotenv').config();
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const encoding = require('encoding'); // this is added (but not used) because of a netlify bug not including this file into the CLI.  They will add in a future patch.
 
 const app = express();
 const router = express.Router();
 
-const lawDsPinLocations = require("./lawDsPinLocations.json");
-const lawAsPinLocations = require("./lawAsPinLocations.json");
-const lawDgPinLocations = require("./lawDgPinLocations.json");
-const countryLocation = require("./countryLocation.json");
+const lawDsPinLocations = require('./lawDsPinLocations.json');
+const lawAsPinLocations = require('./lawAsPinLocations.json');
+const lawDgPinLocations = require('./lawDgPinLocations.json');
+const countryLocation = require('./countryLocation.json');
 
-const clientEmail =
-  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL ||
-  process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL;
+const clientEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL;
 
 // regex/replace is there because netlify env on build was turning all /n's to //n's
-const privateKey =
-  process.env.GOOGLE_PRIVATE_KEY_LOCAL ||
-  process.env.REACT_APP_GOOGLE_PRIVATE_KEY.replace(
-    new RegExp("\\\\n", "g"),
-    "\n"
-  );
+const privateKey = process.env.GOOGLE_PRIVATE_KEY_LOCAL || process.env.REACT_APP_GOOGLE_PRIVATE_KEY.replace(new RegExp('\\\\n', 'g'), '\n');
 
-const gsIdLawDS =
-  process.env.GOOGLE_SHEET_ID_LAW_DS ||
-  process.env.REACT_APP_GOOGLE_SHEET_ID_LAW_DS;
+const gsIdLawDS = process.env.GOOGLE_SHEET_ID_LAW_DS || process.env.REACT_APP_GOOGLE_SHEET_ID_LAW_DS;
 
-const gsIdLawAS =
-  process.env.GOOGLE_SHEET_ID_LAW_AS ||
-  process.env.REACT_APP_GOOGLE_SHEET_ID_LAW_AS;
+const gsIdLawAS = process.env.GOOGLE_SHEET_ID_LAW_AS || process.env.REACT_APP_GOOGLE_SHEET_ID_LAW_AS;
 
-const gsIdLawDG =
-  process.env.GOOGLE_SHEET_ID_LAW_DG ||
-  process.env.REACT_APP_GOOGLE_SHEET_ID_LAW_DG;
+const gsIdLawDG = process.env.GOOGLE_SHEET_ID_LAW_DG || process.env.REACT_APP_GOOGLE_SHEET_ID_LAW_DG;
 
-const gsIdBodyText =
-  process.env.GOOGLE_SHEET_ID_BODY_TEXT ||
-  process.env.REACT_APP_GOOGLE_SHEET_ID_BODY_TEXT;
+const gsIdBodyText = process.env.GOOGLE_SHEET_ID_BODY_TEXT || process.env.REACT_APP_GOOGLE_SHEET_ID_BODY_TEXT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -56,22 +41,22 @@ async function getGoogleSheetInfo(googleSheetID) {
   const rows = await sheet.getRows();
   const info = rows.map((row) => ({
     country: row.Country,
-    worldRegion: row["World Region"],
-    typeOfLegislation: row["Type of Legislation"],
-    lawSubCategory: row["Law Sub Category/In Text Title"],
-    keyOrganizations: row["Key Organization(s)"],
-    titleOfLaw: row["Title of Law"],
-    description: row["(# of characters = 300)  Description"],
-    webLink: row["Web Link"],
-    bodyText: row["Body Text"],
-    categoryOfLegislation: row["Category of Legislation"],
+    worldRegion: row['World Region'],
+    typeOfLegislation: row['Type'],
+    lawSubCategory: row['Law Sub Category/In Text Title'],
+    keyOrganizations: row['Key Organization(s)'],
+    titleOfLaw: row['Title'],
+    description: row['(# of characters = 300)  Description'],
+    webLink: row['Web Link'],
+    bodyText: row['Body Text'],
+    categoryOfLegislation: row['Category of Legislation'],
   }));
   return info;
 }
 
 // API Calls
 // To reach into Identity law Googlesheet
-router.get("/api/country-data/identity", async (req, res) => {
+router.get('/api/country-data/identity', async (req, res) => {
   let countryInfo;
   try {
     countryInfo = await getGoogleSheetInfo(gsIdLawDS);
@@ -82,7 +67,7 @@ router.get("/api/country-data/identity", async (req, res) => {
 });
 
 // To reach into Autonomous-systems law Googlesheet
-router.get("/api/country-data/autonomous-systems", async (req, res) => {
+router.get('/api/country-data/autonomous-systems', async (req, res) => {
   let countryInfo;
   try {
     countryInfo = await getGoogleSheetInfo(gsIdLawAS);
@@ -93,7 +78,7 @@ router.get("/api/country-data/autonomous-systems", async (req, res) => {
 });
 
 // To reach into Personal Data Governance law Googlesheet
-router.get("/api/country-data/personal-data-governance", async (req, res) => {
+router.get('/api/country-data/personal-data-governance', async (req, res) => {
   let countryInfo;
   try {
     countryInfo = await getGoogleSheetInfo(gsIdLawDG);
@@ -104,7 +89,7 @@ router.get("/api/country-data/personal-data-governance", async (req, res) => {
 });
 
 // To reach into the country body text Googlesheet
-router.get("/api/country-data/country-info", async (req, res) => {
+router.get('/api/country-data/country-info', async (req, res) => {
   let countryBodyText;
   try {
     countryBodyText = await getGoogleSheetInfo(gsIdBodyText);
@@ -115,26 +100,26 @@ router.get("/api/country-data/country-info", async (req, res) => {
 });
 
 // API for country location master list
-router.get("/api/countryLocation", async (req, res) => {
+router.get('/api/countryLocation', async (req, res) => {
   res.send({ countryLocation });
 });
 
 // API for Identity law countries locations for pins
-router.get("/api/law-dsCountryPins", async (req, res) => {
+router.get('/api/law-dsCountryPins', async (req, res) => {
   res.send({ lawDsPinLocations });
 });
 
 // API for Autonomous Systems law countries locations for pins
-router.get("/api/law-asCountryPins", async (req, res) => {
+router.get('/api/law-asCountryPins', async (req, res) => {
   res.send({ lawAsPinLocations });
 });
 
 // API for Personal Data Governance law countries locations for pins
-router.get("/api/law-dgCountryPins", async (req, res) => {
+router.get('/api/law-dgCountryPins', async (req, res) => {
   res.send({ lawDgPinLocations });
 });
 
-app.use("/.netlify/functions/server", router);
+app.use('/.netlify/functions/server', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,31 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/Buttons.scss";
+import '../styles/Buttons.scss';
 
 export default function Buttons(props) {
   return (
     <div>
       <div className="button-container">
-        <h3>Click on a Category to Highlight Country Details </h3>
-        <button
-          id="law-ds"
-          className="button decentralized-systems"
-          onClick={props.changeLaw}
-        >
+        <h3>Click on a Pillar to Highlight Country Details </h3>
+        <button id="law-ds" className="button decentralized-systems" onClick={props.changeLaw}>
           Identity & Decentralized Technologies
         </button>
-        <button
-          id="law-as"
-          className="button autonomous-systems"
-          onClick={props.changeLaw}
-        >
+        <button id="law-as" className="button autonomous-systems" onClick={props.changeLaw}>
           AI & Autonomous Systems
         </button>
-        <button
-          id="law-dg"
-          className="button data-governance"
-          onClick={props.changeLaw}
-        >
+        <button id="law-dg" className="button data-governance" onClick={props.changeLaw}>
           Personal Data Governance
         </button>
       </div>
